82a1ef2164a36b19bc01753b60d65145457e8e97
Steven Spiel <stevenspiel@gmail.com>
2015-06-28 23:14:38 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400' rel='stylesheet' type='text/css'>
+	<meta charset="utf-8" />
+	<title>Durham Violin Lessons</title>
+	<meta name="description" content="With years of experience, Heather Spiel is the perfect beginning and intermediate teacher for students of all ages."/>
+	<link rel="canonical" href="http://durhamviolinlessons.com/violin-lessons-heather-spiel/" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Durham Violin Lessons" />
+	<meta property="og:url" content="http://durhamviolinlessons.com/violin-lessons-heather-spiel/" />
+	<meta property="og:site_name" content="Durham Violin Lessons" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="keywords" content="Violin, Lessons, Durham, NC, North, Carolina">
+	<meta name="author" content="Heather Spiel">
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
+	<link rel="stylesheet" href="css/animate.min.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.easings.min.js"></script>
+	<script type="text/javascript" src="js/scrollReveal.min.js"></script>
+  <script type="text/javascript" src="js/tooltipsy.min.js"></script>
+
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-50858410-1', 'durhamviolinlessons.com');
+	  ga('send', 'pageview');
+
+	</script>
+</head>
+<body class="blog">
+<header>
+	<div id="topbar">
+		<div id="contact-container">
+      <a href="http://www.calendly.com/durhamviolinlessons/consultation" title="Book Free Consultation!" class="communication-tooltip" target="_blank"><img src="images/schedule.png" alt="Schedule" class="contact" id="schedule"></a>
+      <a href="mailto:heather@durhamviolinlessons.com?subject=Interested in Lessons" class="communication-tooltip" title="Email Me"><img src="images/mail.png" alt="Email" class="contact"></a>
+      <img src="images/communicate.png" alt="Contact Info" class="contact communication-tooltip" title="Contact" id="communicate">
+		</div>
+	</div>
+	<div id="basic-info">
+		<table>
+			<tr>
+				<td></td><td><b>Heather Spiel</b></td>
+			</tr>
+			<tr>
+				<td>P:</td><td>
+					<span id="desktop-number">989.600.9773</span>
+					<span id="mobile-number"><a href="tel:9896009773">989.600.9773</a></span>
+				</td>
+			</tr>
+			<tr>
+				<td>E:</td><td><a href="mailto:heather@durhamviolinlessons.com?subject=Interested in Lessons">heathermspiel@gmail.com</a></td>
+			</tr>
+			<tr>
+				<td>A:</td><td><a href="https://maps.google.com/maps?q=35.944625,-78.866442" target="_blank">1533 Ellis Rd.</a></td>
+			</tr>
+		</table>
+	</div>
+</header>
+<div id="fullpage">
+    <section id="main">
+    	<div class="pull-left" id="pull-left">
+    		<div class="pull-left-container" id="pull-left-container">
+   				<center><img src="images/heather-thumb.png" alt="Heather Spiel" class="round thumb"></center>
+    			<img src="images/logo-transparent.png" alt="Durham Violin Lessons" id="logo">
+    			<a href="#" class="learn-more" id="learn-more-button">Learn More</a>
+    		</div>
+    	</div>
+    </section>
+    <section id="philosophy">
+    	<div class="pull-center-container">
+    		<div class="pull-center">
+    			<div class="left seventy philosophy text">
+    				<div class="mobile-image">
+    					<img src="images/philosophy.png" alt="Philosophy" id="philosophy-img-mobile" class="icon">
+    				</div>
+	    			<h1>Philosophy</h1>
+	    			<p>My name is Heather Spiel, and I teach violin lessons in Durham, NC</p>
+		    		<p>It is my goal to teach correct and natural violin technique, while helping students enjoy exploring music. I work with the parent and the student to form an understanding of the violin for meaningful practice time. I mainly teach classical repertoire, but will also focus on note reading, scales, and theory.</p>
+		    		<p>It is also important for me to provide performance opportunities for each of my students so they (and the parents and families) can see how they've grown!</p>
+		    	</div>
+		    	<div class="right thirty icon-container">
+	    			<img src="images/philosophy.png" alt="Philosophy" id="philosophy-img" class="icon" data-scroll-reveal>
+    			</div>
+	    	</div>
+    	</div>
+    </section>
+    <section id="services">
+    	<div class="pull-center-container">
+    		<div class="pull-center">
+		    	<div class="left thirty icon-container">
+		    		<img src="images/music.png" alt="What's Provided" id="provided-img" class="icon" data-scroll-reveal="enter up, move 60px, over 0.5s">
+		    	</div>
+		    	<div class="mobile-image">
+		    		<img src="images/music.png" alt="What's Provided" id="provided-img-mobile" class="icon">
+		    	</div>
+    			<div class="right seventy services text">
+		    		<h1>What’s provided?</h1>
+		    		<p>
+						<b>Beginner:</b><br>
+						Weekly private lessons,<br>
+						Group music classes <br>
+						Performance opportunities
+					</p>
+					<p>
+						<b>Advanced:</b><br>
+						Weekly private lessons<br>
+						Group Lesson/combined masterclass<br>
+						Performance opportunities
+		    		</p>
+		    	</div>
+	    	</div>
+    	</div>
+    </section>
+    <!-- <section id="curriculum">
+    	<div class="pull-center-container">
+    		<div class="pull-center">
+    			<h1>
+    				Curriculum
+    			</h1>
+    			<p>I teach mainly classical literature, with an emphasis on scales, theory, note-reading, and a little “fun” musical supplementation.</p>
+		    	<img src="images/gears.png" alt="What's Provided" id="provided-img" class="icon">
+    		</div>
+    	</div>
+    </section> -->
+    <section id="tuition">
+    	<div class="pull-center-container">
+    		<div class="pull-center text">
+    			<h1>Tuition</h1>
+    			<p>$30 for half hour lesson<br>
+				   $45 for 45-minute lesson</p>
+					<p>Fees for books, strings, and all other violin accessories are not included in the pricing. I am happy to help with finding the right purchases for you and your student.</p>
+    			<!-- <p>- Schedule Widget -</p> -->
+    		</div>
+    	</div>
+    </section>
+    <section id="map-container">
+    	<div id="overlay"></div>
+    	<iframe width="100%"
+    	        height="300"
+    	        frameborder="0"
+    	        style="border:0"
+    	        id="map"
+    	        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206709.5761338852!2d-78.84519980756274!3d35.95095406831441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ace516bddfc007%3A0xc75379f4ea42e713!2s1533+Ellis+Rd!5e0!3m2!1sen!2sus!4v1399598046612">
+    	</iframe>
+    	<div id="cover-up"></div>
+    </section>
+    <section id="exploration">
+       	<div class="pull-center-container">
+    		<div class="pull-center">
+    			<div class="left seventy text">
+			    	<h1>Toddler exploring music class</h1>
+			    	<p>One group class each week.</p>
+			    	<p>Forty-five minutes of interaction and play, We will explore music while singing, clapping, Story-telling, and playing. You will be amazed at the connection your toddler will make with the music.</p>
+    			</div>
+    			<div class="right thirty icon-container">
+    				<img src="images/star.png" alt="Toddler Class" id="home-img" class="icon" data-scroll-reveal>
+    			</div>
+			</div>
+		</div>
+    </section>
+    <section class="full-image" id="heather-playing">
+    </section>
+    <section id="about">
+    	<div class="pull-center-container">
+    		<div class="pull-center text">
+    			<h1>About Me</h1>
+    			<p>Violin study began at the age of four in my family. From All-State to small group ensembles I have been involved in music a lot. I enjoyed doing this so much I decided to go to Brigham Young University-Idaho to continue my musical study. I was concert mistress of two of the top orchestras and learned so much from my fellow colleagues there.</p>
+    			<p>After graduating with my Bachelor of Arts in Music I went on to play in the Idaho Falls Symphony and begin a studio of my own in Idaho Falls and Rexburg, Idaho.</p>
+				<p>We have planted our family here in Durham and are thrilled with the artistic community we are a part of in the Raleigh-Durham area. I have taught violin for over seven years, and hope to teach your students in the coming years here in North Carolina.</p>
+    		</div>
+    	</div>
+    </section>
+</div>
+
+<script>
+	function scrollToAnchor(aid){
+	    var anchor = $(aid);
+	    $('html,body').animate({scrollTop: anchor.offset().top},1200,'easeInOutCubic');
+	}
+	$("#learn-more-button").click(function() {
+	   scrollToAnchor('#philosophy');
+	});
+
+	$(document).ready(function(){
+		$('.icon-container').each(function(e){
+			$(this).css('line-height', $(this).siblings('.text').height() + 'px');
+		});
+
+		$('#topbar').hide()
+
+    $('.communication-tooltip').tooltipsy({
+      offset: [0, 10],
+      show: function (e, $el) {
+        $el.show();
+        if($el.attr('id') != 'contact') {
+          $('#basic-info').fadeOut();
+        }
+      },
+      css: {
+        'background': '#ffffff',
+        'padding': '12px 12px 11px 10px',
+        'border': '1px solid rgba(0, 0, 0, 0.2)',
+        'border-radius': '4px'
+      }
+    });
+
+		if ($(window).width > 622) {
+			$("#basic-info").css('left', ($(window).width() / 2) - ($("#basic-info").width()/ 2) + 40) + 'px';
+		} else {
+			$("#basic-info").css('left', ($(window).width() / 2) - ($("#basic-info").width()/ 2) - 16) + 'px';
+		}
+
+		$(document).on('click',function(e){
+			if(!$(e.target).closest('#basic-info, .contact').length) {
+		        if($('#basic-info').is(":visible")) {
+		            $('#basic-info').fadeOut()
+		        }
+		    }
+		});
+
+		$("#communicate").on('click', function(){
+			$("#basic-info").fadeIn()
+		})
+
+		if ($(window).width() < 622){
+			$('#pull-left-container').removeClass('pull-left-container')
+			$('#pull-left').removeClass('pull-left')
+			$(".icon").hide()
+		} else {
+			$('#pull-left-container').addClass('pull-left-container')
+			$('#pull-left').addClass('pull-left')
+			$(".icon").show()
+		}
+	});
+
+	$(window).resize(function(){
+		$('.icon-container').each(function(e){
+			$(this).css('line-height', $(this).siblings('.text').height() + 'px');
+		});
+
+		if($(window).scrollTop() < ($("#main").height() * 1.15)){
+			$('#topbar').hide()
+		}
+
+		if ($(window).width > 622) {
+			$("#basic-info").css('left', ($(window).width() / 2) - ($("#basic-info").width()/ 2) - 40) + 'px';
+		} else {
+			$("#basic-info").css('left', ($(window).width() / 2) - ($("#basic-info").width()/ 2) - 16) + 'px';
+		}
+
+
+		if ($(window).width() < 622){
+			$('#pull-left-container').removeClass('pull-left-container')
+			$('#pull-left').removeClass('pull-left')
+			$(".icon").hide()
+		} else {
+			$('#pull-left-container').addClass('pull-left-container')
+			$('#pull-left').addClass('pull-left')
+			$(".icon").show()
+		}
+	})
+
+	$(window).scroll(function() { // when the page is scrolled run this
+	    if($(this).scrollTop() != 0 && $(this).scrollTop() >= ($("#main").height() * 1.15)) { // if you're NOT at the top
+	        $('#topbar').fadeIn();
+	    } else { // else
+	        $('#topbar').fadeOut();
+	    }
+	});
+
+
+
+</script>
+<script>
+	window.scrollReveal = new scrollReveal();
+</script>
+
+<div class="clear"></div>
+<footer id="footer">
+	<div class="center">
+		Copyright ©2014 <a href="https://plus.google.com/112095517711920987206?rel=author">Heather Spiel</a>
+		<div class="clear"></div>
+	</div>
+	<div class="clear"></div>
+</footer>
+</section>
+</body>
+</html>
